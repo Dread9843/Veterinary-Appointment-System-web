931b49af7e0fd589cdcefb6a8c3d2cad4e0ddf86
alnizastha <118263378+alnizastha@users.noreply.github.com>
2022-11-27 19:02:12 +0545

    vetcare

---
+++ about.php
@@ -0,0 +1,15 @@
+<div class="col-12">
+    <div class="row my-5">
+        <div class="col-md-8">
+            <div class="card rounded-0 card-outline shadow" >
+                <div class="card-body rounded-0">
+                    <h2 class="text-center">About</h2>
+                    <center><hr class="bg-navy border-navy w-25 border-2"></center>
+                    <div>
+                        <?= file_get_contents("about_us.html") ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
